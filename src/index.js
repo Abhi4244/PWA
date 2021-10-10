4b7caca171a9932277d3ceb8d605e5a7f026e985
Abhi4244 <72958703+Abhi4244@users.noreply.github.com>
2021-10-10 22:37:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-
+if ("serviceWorker" in navigator){
+  navigator.serviceWorker.register("sw.js").then(registration => {
+    console.log("SW registered!");
+    console.log(registration);
+  }).catch(error=> {
+    console.log("SW registration faileed");
+    console.log(error);
+  });
+}
