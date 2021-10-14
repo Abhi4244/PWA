edfaa22c637082441cb5b3f379f3df493aae3c07
Abhi4244 <72958703+Abhi4244@users.noreply.github.com>
2021-10-14 16:49:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", e=> {
-    console.log("install");
+ e.waitUntill(
+     caches.open("static").then(cache => {
+         return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
+     })
+     );
 });
