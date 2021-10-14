16354325281d5ec5023eeb626b1bfa4735635c3b
Abhi4244 <72958703+Abhi4244@users.noreply.github.com>
2021-10-14 16:57:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("install", e=> {
- e.waitUntill(
+ e.waitUntil(
      caches.open("static").then(cache => {
          return cache.addAll(["./", "./src/master.css", "./images/logo192.png"]);
      })
